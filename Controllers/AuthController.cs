72cc3a7a38d66909f8a92a41d44e1237277e7508
Kristijan Maoduš <kmaodus@foi.hr>
2020-07-26 21:04:06 +0200

    Added user login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -20,9 +20,20 @@ namespace dotnet_rpg.Controllers
         [HttpPost("Register")]
         public async Task<IActionResult> Register(UserRegisterDto request)
         {
-            ServiceResponse<int> response = await _authRepo.Register(
-                new User { Username = request.Username }, request.Password
-                );
+            ServiceResponse<int> response = await _authRepo.Register(new User { Username = request.Username }, request.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginDto request)
+        {
+            ServiceResponse<string> response = await _authRepo.Login(request.Username, request.Password);
+
             if (!response.Success)
             {
                 return BadRequest(response);
--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -14,9 +14,27 @@ namespace dotnet_rpg.Data
         }
 
 
-        public Task<ServiceResponse<string>> Login(string username, string password)
+        public async Task<ServiceResponse<string>> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            ServiceResponse<string> response = new ServiceResponse<string>();
+            User user = await _context.Users.FirstOrDefaultAsync(x => x.Username.ToLower().Equals(username.ToLower()));
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User not found!";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Wrong password";
+            }
+            else
+            {
+                response.Data = user.Id.ToString();
+                response.Message = "Logged in!";
+            }
+
+            return response;
         }
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
@@ -55,6 +73,22 @@ namespace dotnet_rpg.Data
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
         }
 
